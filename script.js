73fae8c2ca68605bc728391599811cb2ea9ea36a
jaypawar2004 <pawarjay7000@gmail.com>
2025-03-27 15:33:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// let a = 12 
+// let b = "1"
+// let c = 5
+// //  console.log(typeof(a+b+c))
+
+
+
+//  let arr = [1,2,3,4,5]
+// //  console.log(arr.length)
+
+
+var copy =  "hello dosto"
+
+for(var i = 0; i< 10; i++){
+    console.log(copy)
+}
