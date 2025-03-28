6c09904bb9a3027f8c73fd251b3d9492e83ce378
jaypawar2004 <pawarjay7000@gmail.com>
2025-03-28 22:52:39 +0530

    Add Loop

---
+++ script03.js
@@ -0,0 +1,40 @@
+//Using Loop method
+
+//loop 2 type ke hote hai
+// 1.. Entry controlled loop
+// -- for loop
+// -- while loop
+// 2.. Exit controlled loop
+// -- do while loop
+
+
+// for loop me 3 parameter hote hai
+// 1.. declare/initialization
+// 2.. condition
+// 3.. increment or decrement / Change
+
+
+// for loop
+
+// for(var i = 0; i<=5; i++){
+//     console.log(i)
+// }
+
+
+var num = Number(prompt("Enter a number"));
+
+if (isNaN(num)) {
+  console.log("Invalid input")
+    
+}else{
+    let i;
+    
+    for( i = 0 ; i<num ; i++){
+        console.log("hello dost ")
+    }
+    
+    
+    console.log(i)
+}
+
+
